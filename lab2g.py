a8ed8655348b6fd6062577335738d7b0ae048f04
johnmarques85 <joaofilho@me.com>
2026-05-21 09:30:16 -0400

    Add lab2g.py

---
+++ lab2g.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+timer = 3
+
+if len(sys.argv) == 2:
+    timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
